9c1b0bd3b276b5defcb6df5a54d7345ddbbc687f
root <root@LAPTOP-CIFFJCB4.localdomain>
2024-11-26 18:55:27 +0100

    added the necessary commentaries

--- print_string.c
+++ print_string.c
@@ -11,10 +11,16 @@
 int print_string(va_list args)
 {
 	char *string = va_arg(args, char *);
+	/* Initialisation of a listing structure */
+
 	int length = 0;
 
 	while (*string)
 	{
+		/*
+		 * Prints the pointer of our string, as long as it exists
+		 * And increment the string and it's length
+		 */
 		_putchar(*string);
 		length++;
 		string++;
