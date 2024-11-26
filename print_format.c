9b22d267ba6ebe25dc76ee8763fe681974813551
root <root@LAPTOP-CIFFJCB4.localdomain>
2024-11-26 20:36:15 +0100

    transferred the content of print_string.c to print_format.c

--- print_format.c
+++ print_format.c
@@ -15,6 +15,36 @@ int print_character(va_list args)
 	return (1);
 }
 
+
+/**
+ * print_string - prints several chracter to form a string
+ *
+ * @args: list of arguments
+ *
+ * Return: the length of printed charecter
+ */
+
+int print_string(va_list args)
+{
+	char *string = va_arg(args, char *);
+	/* Initialisation of a listing structure */
+
+	int length = 0;
+
+	while (*string)
+	{
+		/*
+		 * Prints the pointer of our string, as long as it exists
+		 * And increment the string and it's length
+		 */
+		_putchar(*string);
+		length++;
+		string++;
+	}
+
+	return (length);
+}
+
 /**
  * print_integer - prints an integer that can be a number or a digit
  *
