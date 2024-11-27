1f38e7062d8b6b70c7ffd9aae11d5c1ca4a46cd4
hls <hilliassmirali@gmail.com>
2024-11-27 14:32:05 +0100

    add return feature to get the length of printed character on each function

--- print_format.c
+++ print_format.c
@@ -6,10 +6,58 @@
  * Return: void
  */
 
-void print_c(va_list args)
+int print_c(va_list args)
 {
+	int length = 0;
 	char temporyVariable;
 
 	temporyVariable = (char)va_arg(args, int);
 	_putchar(temporyVariable);
+
+	return (length++);
+}
+
+/**
+ * print_d - prints element of type integer
+ * @args: type structure
+ * Return: void
+ */
+
+int print_d(va_list args)
+{
+	int number;
+	int length = 0;
+	char temporaryArray[100];
+	int indexOfArray = 0;
+
+	number = va_arg(args, int);
+
+	if (number == 0)
+	{
+		_putchar('0');
+		length++;
+	}
+
+	if (number < 0)
+	{
+		_putchar('-');
+		length++;
+		number = -number;
+	}
+
+	while (number >  0)
+	{
+		temporaryArray[indexOfArray++] = (number % 10) + '0';
+		number /= 10;
+	}
+
+	while (indexOfArray >= 0)
+	{
+		_putchar(temporaryArray[indexOfArray]);
+		indexOfArray--;
+		length++;
+	}
+
+	return (length);
+
 }
